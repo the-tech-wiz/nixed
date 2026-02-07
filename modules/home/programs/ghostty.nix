9b8f235ad2e548f279f5471f63aeb6aa3d05a0b6
lel4 <long.l.le@aalto.fi>
2026-02-07 17:49:24 +0200

    ghostty position

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -6,7 +6,9 @@
         confirm-close-surface = false;
         window-decoration = "server";
         window-height = 10000;
-        window-width=10000;
+        window-width = 10000;
+        window-padding-x = 0;
+        window-padding-y = 0;
       };
     };
   };
