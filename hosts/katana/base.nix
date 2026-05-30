89f84a66cecf22a10d05c41491016374f844a074
techwiz <thetechwizard@proton.me>
2026-05-30 13:47:18 +0300

    removed and moved junk

--- hosts/katana/base.nix
+++ hosts/katana/base.nix
@@ -51,19 +51,9 @@
       LC_TIME = "fi_FI.UTF-8";
     };
 
-    # Enable the X11 windowing system.
-    # You can disable this if you're only using the Wayland session.
-    # services.xserver.enable = true;
-
     # Enable the KDE Plasma Desktop Environment.
     services.displayManager.sddm.enable = true;
     services.desktopManager.plasma6.enable = true;
-
-    # Configure keymap in X11
-    # services.xserver.xkb = {
-    #   layout = "us";
-    #   variant = "";
-    # };
 
     # Enable CUPS to print documents.
     services.printing.enable = true;
@@ -76,12 +66,6 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
     };
 
     # Enable touchpad support (enabled default in most desktopManager).
@@ -92,17 +76,11 @@
       isNormalUser = true;
       description = "TechWiz";
       extraGroups = ["networkmanager" "wheel"];
-      packages = with pkgs; [
-        # kdePackages.kate
-        #  thunderbird
-      ];
       shell = pkgs.fish;
     };
     # for default shell
     programs.fish.enable = true;
 
-    # Install firefox.
-    programs.firefox.enable = true;
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
@@ -115,28 +93,7 @@
     # $ nix search wget
     environment.systemPackages = with pkgs; [
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      git
-      #  wget
     ];
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
@@ -147,5 +104,14 @@
     system.stateVersion = "25.11"; # Did you read the comment?
 
     nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    # services.xserver.enable = true;
+    # Configure keymap in X11
+    # services.xserver.xkb = {
+    #   layout = "us";
+    #   variant = "";
+    # };
   };
 }
