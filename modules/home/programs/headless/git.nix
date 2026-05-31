bb59421b6bdb1cd41a0f1b3ba4c752527ff6c662
techwiz <thetechwizard@proton.me>
2026-05-31 14:19:18 +0300

    refactored git and diffing

--- modules/home/programs/headless/git.nix
+++ modules/home/programs/headless/git.nix
@@ -1,26 +1,37 @@
 {
   flake.modules.homeManager.programs = _: {
     # the friendly interactive shell.
-    programs.git = {
-      enable = true;
-      settings = {
-        user = {
-          name = "techwiz";
-          email = "thetechwizard@proton.me";
-          signingKey = "";
+    programs = {
+      git = {
+        enable = true;
+        settings = {
+          user = {
+            name = "techwiz";
+            email = "thetechwizard@proton.me";
+          };
+          core.autocrlf = false;
+          init.defaultBranch = "main";
+          push.autoSetupRemote = true;
         };
-        core.autocrlf = false;
-        init.defaultBranch = "main";
-        push.autoSetupRemote = true;
+        signing = {
+          format = "ssh";
+          signByDefault = true;
+          key = "/home/techwiz/.ssh/id_ed25519";
+        };
+      }; # syntax-aware diff
+      difftastic = {
+        enable = true;
+        git = {
+          enable = true;
+          diffToolMode = true;
+        };
       };
-      signing = {
-        format = "ssh";
-        signByDefault = true;
+      lazygit = {
+        enable = true;
+        settings = {
+          git.pagers.useExternalDiffGitConfig = true;
+        };
       };
-    };
-    programs.lazygit = {
-      enable = true;
-      settings = {};
     };
   };
 }
