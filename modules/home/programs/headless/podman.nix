34db132ab22ee7574c5d3062975b8fb64dd22a30
techwiz <thetechwizard@proton.me>
2026-06-12 16:31:27 +0300

    refactored/expanded vms and containers

--- modules/home/programs/headless/podman.nix
+++
@@ -1,7 +0,0 @@
-{
-  flake.modules.homeManager.programs = _: {
-    services.podman = {
-      enable = true;
-    };
-  };
-}
--- modules/nixos/virtualisation/vm.nix
+++ modules/nixos/virtualisation/vm.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{
   flake.modules.nixos.virtualization = {
     config,
     pkgs,
