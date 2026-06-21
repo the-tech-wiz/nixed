cc3f253f6ef720d8c3b9a01a443d19b4c6ed82b0
techwiz <thetechwizard@proton.me>
2026-06-21 13:19:04 +0300

    merged input and keyd, now inserts unicode chars

--- modules/nixos/base/input.nix
+++ modules/nixos/base/input.nix
@@ -1,13 +1,50 @@
 {
   flake.modules.nixos.base = {pkgs, ...}: {
-    # for vietnamese
     i18n.inputMethod = {
       type = "fcitx5";
       enable = true;
       fcitx5.addons = with pkgs; [
         fcitx5-gtk
+        # for vietnamese
         kdePackages.fcitx5-unikey
       ];
     };
+    # keyd remaps for capslock replacement + finnish keys
+    services.keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = ["*"]; # what goes into the [id] section, here we select all keyboards
+          # Everything but the ID section:
+          settings = {
+            # The main layer, if you choose to declare it in Nix
+            main = {
+              pageup = "sysrq";
+              capslock = "overload(control,esc)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
+            };
+
+            control = {
+              a = "ä";
+              o = "ö";
+              e = "€";
+            };
+            "shift+control" = {
+              o = "Ö";
+              a = "Ä";
+              e = "€";
+            };
+          };
+          extraConfig = ''
+            # put here any extra-config, e.g. you can copy/paste here directly a configuration, just remove the ids part
+            # grumble grumble the archaic keys the graphics course needs fsr
+            # kp1 = end
+            # kp3 = pagedown
+            # kp7 = home
+            # kp9 = pageup                    '';
+        };
+      };
+    };
+    # enable unicode support
+    environment.sessionVariables."XCOMPOSEFILE" = "${pkgs.keyd}/share/keyd/keyd.compose";
   };
 }
