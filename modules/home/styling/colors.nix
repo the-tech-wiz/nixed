3df1fffc62c45dbe9f98c40ef9074a0da8a395b2
techwiz <thetechwizard@proton.me>
2026-06-27 14:50:10 +0300

    cleaned up theming

--- modules/home/styling/colors.nix
+++ modules/home/styling/colors.nix
@@ -1,32 +1,17 @@
-{inputs, ...}: {
-  flake.modules.homeManager.styling = {
-    config,
-    pkgs,
-    ...
-  }: let
-    flavor = "mocha";
-    accent = "sky";
-  in {
-    imports = [inputs.catppuccin.homeModules.catppuccin];
-    catppuccin = {
-      autoEnable = true;
-      enable = true;
-      # antigravity.enable = false;
-      # qt5ct.enable = true;
-      # vscode.profiles.default.enable = false;
-      # vscodium.profiles.default.enable = false;
-
-      inherit accent flavor;
-
-      # Catppuccin's GTK module is basically dead, disable
-      # it until this issue is resolved.
-      #
-      # <https://github.com/catppuccin/gtk/issues/262>
-      # gtk.icon.enable = false;
+{ inputs, ... }: {
+  flake.modules.homeManager.styling =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ inputs.catppuccin.homeModules.catppuccin ];
+      catppuccin = {
+        autoEnable = true;
+        enable = true;
+        flavor = "mocha";
+        accent = "sky";
+      };
     };
-    # programs.vscodium.profiles.default = {
-    #   extensions = with pkgs.vscode-extensions; [catppuccin.catppuccin-vsc];
-    #   userSettings = {"catppuccin.accentColor" = accent;};
-    # };
-  };
 }
