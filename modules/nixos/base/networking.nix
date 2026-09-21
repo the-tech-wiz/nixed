9162f2eb67ede791bab126ba1a8ac9e08a0cba63
techwiz <thetechwizard@proton.me>
2026-09-21 22:10:52 +0300

    added openconnect for aalto VPN

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -1,17 +1,22 @@
 {
-  flake.modules.nixos.base = {
+  flake.modules.nixos.base = {pkgs, ...}: {
     # enable networking
-    networking.networkmanager.enable = true;
-
-    # kde connect port forwarding
-    networking.firewall = rec {
-      allowedTCPPortRanges = [
-        {
-          from = 1714;
-          to = 1764;
-        }
-      ];
-      allowedUDPPortRanges = allowedTCPPortRanges;
+    networking = {
+      networkmanager = {
+        enable = true;
+        # aalto cisco anyconnect VPN
+        plugins = with pkgs; [networkmanager-openconnect];
+      };
+      # KDE Connect
+      firewall = rec {
+        allowedTCPPortRanges = [
+          {
+            from = 1714;
+            to = 1764;
+          }
+        ];
+        allowedUDPPortRanges = allowedTCPPortRanges;
+      };
     };
   };
 }
